c2fbb7379b50ed47459570b4d510484e362f87a5
Vikas-18 <vikaskohli18impossible@gmail.com>
2023-05-23 12:29:25 +0530

    changes made

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,41 +1,27 @@
 import React from "react";
-import "./styles.css";
-import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function HomePage() {
   return (
     <>
-      <div className="nav">
-        <input type="checkbox" id="nav-check" />
-        <div className="nav-header">
-          <div className="nav-title">IPL INFO 2008-2020</div>
-        </div>
-        <div className="nav-btn">
-          <label htmlFor="nav-check">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
-        </div>
-        <div className="nav-links">
-          <Link to="/purplecap">PurpleCap</Link>
-          <Link to="/orangecap">OrangeCap</Link>
-          <Link to="/finals">Finals</Link>
-          <Link to="/longestsix">Longest Six</Link>
-          <Link to="/emergingplayer">Emerging Player</Link>
-        </div>
-      </div>
-      <h1
+      <NavBar />
+      <div
         style={{
-          textAlign: "center",
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "calc(100vh - 64px)", // Subtracting the height of the NavBar
         }}
       >
-        WELCOME TO IPL INFO HERE YOU WILL GET INFO ABOUT IPL FROM 2008-2020
-      </h1>
+        <h1
+          style={{
+            textAlign: "center",
+            width: "50%",
+          }}
+        >
+          WELCOME TO IPL INFO HERE YOU WILL GET INFO ABOUT IPL FROM 2008-2020
+        </h1>
+      </div>
     </>
   );
 }
